b2a07cecb69bc032bd3d9080b15b5e8b2cc23096
jaasonw <jayywong92@gmail.com>
2023-02-10 22:57:55 -0800

    make dropdown wider?

--- src/lib/RowSelector.svelte
+++ src/lib/RowSelector.svelte
@@ -4,7 +4,7 @@
   const rowNums = [...Array(rows).keys()];
 </script>
 
-<select class="h-9 w-9 rounded-md" {name} id="add">
+<select class="h-9 rounded-md" {name} id="add">
   {#each rowNums as row}
     <option value={row}>R{row + 1}</option>
   {/each}
