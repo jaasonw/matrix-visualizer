f4dfd7c2da936ed97d30ed3e2a5ebc1f91ef3100
jaasonw <jayywong92@gmail.com>
2023-02-11 01:43:35 -0800

    fix footer text alignment

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,7 +1,7 @@
 <footer class="grid grid-cols-3 w-full mt-4 text-gray-400 text-xs">
   <span class="flex flex-col items-center justify-center col-start-2">
     <span>copyright 2023</span>
-    <span
+    <span class="text-center"
       >open source under <a
         class="underline"
         href="https://github.com/jaasonw/matrix-visualizer/blob/main/LICENSE"
@@ -12,9 +12,9 @@
   <span class="flex flex-col items-end">
     <a
       href="https://github.com/jaasonw/matrix-visualizer"
-      class="hover:underline">view source on github</a
+      class="text-right hover:underline">view source on github</a
     >
-    <a href="https://jason-wong.me/donate" class="hover:underline"
+    <a href="https://jason-wong.me/donate" class="text-right hover:underline"
       >buy me a coffee</a
     >
   </span>
