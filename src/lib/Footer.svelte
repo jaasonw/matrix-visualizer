cb32094bcfb9ce24b072b44890a89e8b1c45c964
jaasonw <jayywong92@gmail.com>
2023-02-10 22:22:53 -0800

    fix build(?) and links

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,10 +1,21 @@
 <footer class="grid grid-cols-3 w-full mt-4 text-gray-400 text-xs">
   <span class="flex flex-col items-center justify-center col-start-2">
     <span>copyright 2023</span>
-    <span>open source under MIT license</span>
+    <span
+      >open source under <a
+        class="underline"
+        href="https://github.com/jaasonw/matrix-visualizer/blob/main/LICENSE"
+        >MIT license</a
+      ></span
+    >
   </span>
   <span class="flex flex-col items-end">
-    <a href="" class="hover:underline">view source on github</a>
-    <a href="" class="hover:underline">buy me a coffee</a>
+    <a
+      href="https://github.com/jaasonw/matrix-visualizer"
+      class="hover:underline">view source on github</a
+    >
+    <a href="https://jason-wong.me/donate" class="hover:underline"
+      >buy me a coffee</a
+    >
   </span>
 </footer>
